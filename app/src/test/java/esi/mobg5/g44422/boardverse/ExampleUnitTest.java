7849b75839ef98ba651f1a9714181da5ff0689c8
BrandtMatthieu <44422@etu.he2b.be>
2020-01-04 16:12:05 +0100

    Added config files

---
+++ app/src/test/java/esi/mobg5/g44422/boardverse/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package esi.mobg5.g44422.boardverse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
